031c26caedd9cc469248ce5a197fa56e71888d28
james francis toy iv <pct@opensesame.st>
2010-02-17 12:18:48 -0500

    hw7: i _think_ resynth is done now.

--- hw7/resynth.m
+++ hw7/resynth.m
@@ -4,17 +4,19 @@ function y = resynth(freqs, coeffs, len)
 %	and a length as produced by COMPRESSAUDIO.  It then synthesizes the corresponding
 %	signal which is returned in 'y'.
 
-% setup
-Y(freqs(1:length(freqs))) = coeffs(1:length(coeffs)); % fill Y with the coefficients and order based on freqs vector
-Y = real(Y); % convert everything to reals
-Y = Y * 2; % account for negative freqs deleted in compress
+% setup: init and fill Y with the coefficients and order based on freqs vector
+Y = zeros(len); % init and allocate mem for 
+Y(freqs(1:length(freqs))) = coeffs(1:length(coeffs));
+
+% ifft, coversion to real, ensure span[-1,1],  and account for compressaudio's dropped negative freqs
+y = ifft(Y); % inverse fast fourier transform
+y = real(y); % convert everything to reals
 
 % rescale if necessary
-if max(abs(Y)) > 1
-	Y = Y ./ length(Y);
+if max(abs(y)) > 1
+        y = y ./ max(abs(y)); %ALTERNATE :: -1 + (1 - (-1)) * (value - min)/(max-min)
 end % if
 
-% inverse fast fourier transform
-y = ifft(Y);
+y = [y y];   % account for negative freqs deleted in compress
 
 end % resynth
