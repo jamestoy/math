542f3ffa4a8a872578307cf61f1b2405940e83bf
james francis toy iv <pct@opensesame.st>
2010-03-10 09:54:02 -0500

    hw9: README and main script mods

--- hw9/main.m
+++ hw9/main.m
@@ -1,12 +1,13 @@
 %% james francis toy iv
 %% homework 9 main script file
+
 % equation from number 5
 f = @(x) exp(-x.^2);
 
 %givens
 a = 0;
 b = 1;
-n = 3;		 
+n = 5;		 
 n1 = n*2;	 % double n as the hw says
 d0 = 0;		 % this is the derivative of f(x) @ 0
 dn = -2*exp(-1); % this is the derivative of f(x) @ 1
@@ -14,11 +15,16 @@ dn = -2*exp(-1); % this is the derivative of f(x) @ 1
 %two approximations
 k = hermitequad(f,a,b,n,d0,dn);
 k1 = hermitequad(f,a,b,n1,d0,dn);
+diff = k1-k;
+err = norm(diff);
 
 %while the residual is greater than 10e-6
-while ((k - k1) > 10e-6)
+while (err > 10e-6)
 	k = hermitequad(f,a,b,n,d0,dn);
 	k1 = hermitequad(f,a,b,n1,d0,dn);
+
+	n1 = n1 * 2;
+	err = norm(k1-k);
 end % while
 
-residual = k - k1
+err
